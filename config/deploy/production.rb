702eb86fcc22228d44fe5cad5ff96ee2b9f0c68a
gxbsst <gxbsst@gmail.com>
2015-02-05 14:55:18 +0800

    使静态文件能被nginx找到。

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,17 @@
 set :rails_env, 'production'
 set :deploy_to, "/home/deployer/production/pbl_sso"
 
+server '10.10.31.19',
+       user: 'deployer',
+       roles: %w{web app db},
+       ssh_options: {
+           user: 'deployer', # overrides user setting above
+           #keys: %w(/home/user_name/.ssh/id_rsa),
+           #forward_agent: true,
+           auth_methods: %w(password),
+           password: '51448888'
+       }
+
 server '10.10.31.112',
        user: 'deployer',
        roles: %w{web app db whenever},
